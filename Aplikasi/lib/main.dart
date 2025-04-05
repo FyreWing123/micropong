2d765d71d35bc35dff4ca507aa765ede4c87ffee
FyreWing123 <160138955+FyreWing123@users.noreply.github.com>
2025-04-05 14:24:34 +0700

    firebase

--- Aplikasi/lib/main.dart
+++ Aplikasi/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:aplikasi/LoginScreen/welcome.dart';
+import 'package:aplikasi/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import './LoginScreen/welcome.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
