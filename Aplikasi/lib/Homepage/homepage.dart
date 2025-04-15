ae584e1af8f4f73d09d3f3a74a66b35328e301f4
Ahmad Reza Fadlan <ahmadrz1707@gmail.com>
2025-04-15 11:51:51 +0700

    update cu

--- Aplikasi/lib/Homepage/homepage.dart
+++ Aplikasi/lib/Homepage/homepage.dart
@@ -54,8 +54,7 @@ class _HomepageState extends State<Homepage> {
             ),
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: ListView(
           children: [
             SizedBox(
               height: 230,
@@ -74,7 +73,7 @@ class _HomepageState extends State<Homepage> {
                 },
               ),
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 10),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 18),
               child: Text(
@@ -162,123 +161,6 @@ class _HomepageState extends State<Homepage> {
               ),
             ),
             SizedBox(height: 10),
-            SizedBox(
-              height:
-                  MediaQuery.of(context).size.height *
-                  0.7, // atur sesuai layout kamu
-              child: GridView.builder(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // 2 kolom
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
-                  childAspectRatio: 3 / 4, // rasio card: ubah sesuai kebutuhan
-                ),
-                itemCount:
-                    10, // jumlah item, bisa kamu ubah atau pakai data dinamis
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 6,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(16),
-                          ),
-                          child: Image.network(
-                            'https://picsum.photos/200/120?random=$index',
-                            width: double.infinity,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Desain Interior",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.location_on,
-                                    size: 14,
-                                    color: Colors.grey,
-                                  ),
-                                  Text(
-                                    " Surabaya",
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.grey.shade600,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 4),
-                              Text(
-                                "Muhammad Iqbal Rahmananda",
-                                style: TextStyle(fontSize: 12),
-                              ),
-                              SizedBox(height: 6),
-                              Text(
-                                "start from",
-                                style: TextStyle(
-                                  fontSize: 11,
-                                  color: Colors.grey,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              Text(
-                                "Rp100,000",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.purple,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Spacer(),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Container(
-                            margin: EdgeInsets.all(10),
-                            padding: EdgeInsets.all(6),
-                            decoration: BoxDecoration(
-                              color: Colors.purple,
-                              shape: BoxShape.circle,
-                            ),
-                            child: Icon(
-                              Icons.arrow_forward,
-                              size: 18,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
           ],
         ),
         bottomNavigationBar: CustomNavbar(
